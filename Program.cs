1cd633d02d96f84f9f9b4e05f24adfb5cc4887f1
Luz Fernández Acuña <luz.fernandezac@gmail.com>
2022-04-21 21:32:36 -0500

    ejemplo2

--- Program.cs
+++ Program.cs
@@ -6,9 +6,30 @@ namespace portafolioC_
     {
         static void Main(string[] args)
         {
-            string nombre = "Luz A";
-            Console.WriteLine("Hello World! - " + nombre);
-            Console.WriteLine($"Hello World! - {nombre}" );
+            /*int num1, num2, res;
+            Console.WriteLine("Digite el primer numero");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el segundo numero");
+            num2 = int.Parse(Console.ReadLine());
+
+            res = num1 + num2;
+            Console.WriteLine($"El primer numero es {num1}" );
+            Console.WriteLine($"El segundo numero es {num2}" );
+            Console.WriteLine($"La suma de los numeros es: {num1} + {num2} = {res}" );*/ 
+
+            int cantidad, cobro, pago;
+
+            Console.WriteLine("Digite la cantidad de productos a vender");
+            cantidad = int.Parse(Console.ReadLine());
+            
+            cobro = cantidad*10000;
+            Console.WriteLine($"El valor de la venta es {cobro}");
+
+            Console.WriteLine($"Ingrese el monto con el quie paga el cliente" );
+            pago = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"El cambio que debe entregar al cliente es: {pago - cobro}" );
         }
     }
 }
