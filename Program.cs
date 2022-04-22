f98c44a9ce20153ad1a9d9a6561a2928e22a1e77
Luz Fernández Acuña <luz.fernandezac@gmail.com>
2022-04-21 20:39:36 -0500

    Se inicia proyecto base

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace portafolioC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Luz A";
+            Console.WriteLine("Hello World! - " + nombre);
+            Console.WriteLine($"Hello World! - {nombre}" );
+        }
+    }
+}
+
+
